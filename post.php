6c263eeb958477d10db235fe23e955d581ecb28f
ClanMax <me@clanmax.ru>
2016-10-31 22:23:19 +0300

    2016 landing page. initial commit

---
+++ post.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+
+
+$message = "Имя: $name \nE-mail: $email \n ----------------- \nТелефон: $tel";
+
+
+// На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+$message = wordwrap($message, 70);
+
+// Отправляем
+mail('woodsk@mail.ru', 'Мой сайт', $message, "Content-type: text/plain; charset=utf-8");
+?>
